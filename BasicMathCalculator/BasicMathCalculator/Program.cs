bc9893ba2ea8fd6de1ab7755a05efe04aa435987
Aron James delos Reyes <ajms.072799@gmail.com>
2020-10-15 00:19:26 +0800

    added a funtion for prompting the math operator

--- BasicMathCalculator/BasicMathCalculator/Program.cs
+++ BasicMathCalculator/BasicMathCalculator/Program.cs
@@ -10,6 +10,22 @@ namespace BasicMathCalculator
     {
         static void Main(string[] args)
         {
+            // Basic Math Calculator App
+            PromptingTheMathOperator();
+
+            // <~ End of the program
+            Console.ReadLine();
+        }
+
+        static char PromptingTheMathOperator()
+        {
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("\t\t Basic Math Calculator");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.Write("Select an Operator(+, -, *, /): ");
+            char mathOperator = Convert.ToChar(Console.ReadLine());
+
+            return mathOperator;
         }
     }
 }

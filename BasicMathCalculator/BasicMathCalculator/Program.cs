2b596f3a6ca65977c565aebbd906cbed0ebd7ce0
Aron James delos Reyes <ajms.072799@gmail.com>
2020-10-16 22:10:19 +0800

    Done the experiment of adding the another trasaction in basic math calculator

--- BasicMathCalculator/BasicMathCalculator/Program.cs
+++ BasicMathCalculator/BasicMathCalculator/Program.cs
@@ -12,10 +12,29 @@ namespace BasicMathCalculator
 
         static void Main(string[] args)
         {
-            int ResultOfTheMathComputation = BasicMathComputation();
+            int NumberOfTransaction, i;
+            NumberOfTransaction = 1;
+            i = 0;
 
-            Console.WriteLine(ResultOfTheMathComputation);
+            do
+            {
+                int ResultOfTheMathComputation = BasicMathComputation(); // Getting the return value and storing to this variable.
+
+                Console.WriteLine("Basic Computation Result: " + ResultOfTheMathComputation); // Printting the variable that you get.
+
+                Console.Write("Do you want another transaction?(Y/n): "); // input for getting the another transaction.
+                char AnswerForAnotherTransaction = Convert.ToChar(Console.ReadLine()); // storing the answer for another transaction in this variable.
+
+                if (AnswerForAnotherTransaction == 'Y' || AnswerForAnotherTransaction == 'y')
+                {
+                    NumberOfTransaction += NumberOfTransaction;
+                }
+                else {
+                    Console.WriteLine("Thank you for using this Basic Math Calculator!");
+                }
+                i++;
             
+            } while (i < NumberOfTransaction);
             // <~ End of the program
             Console.ReadLine();
         }
@@ -24,43 +43,30 @@ namespace BasicMathCalculator
             Console.WriteLine("------------------------------------------------------------");
             Console.WriteLine("\t\t Basic Math Calculator");
             Console.WriteLine("------------------------------------------------------------");
+          
+            Console.Write("Select an Math Operator(+, -, *, /): ");
+            char MathOperator = Convert.ToChar(Console.ReadLine());
 
-            int NumberOfTransaction, i;
-            NumberOfTransaction = 1;
-            i = 0;
+            Console.Write("Enter your first number: ");
+            int FirstNumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            int SecondNumber = Int32.Parse(Console.ReadLine());
 
-            do 
-            {
-                Console.Write("Select an Math Operator(+, -, *, /): ");
-                char MathOperator = Convert.ToChar(Console.ReadLine());
-
-                Console.Write("Enter your first number: ");
-                int FirstNumber = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter your second number: ");
-                int SecondNumber = Int32.Parse(Console.ReadLine());
-
-                switch (MathOperator)
-                { 
-                    case '+':
-                        ComputationResult = FirstNumber + SecondNumber;
-                        break;
-                    case '-':
-                        ComputationResult = FirstNumber - SecondNumber;
-                        break;
-                    case '*':
-                        ComputationResult = FirstNumber * SecondNumber;
-                        break;
-                    case '/':
-                        ComputationResult = FirstNumber / SecondNumber;
-                        break;
-                }
-
-                Console.Write("Do you want another transaction?(Y/n) ");
-                char AnswerForAnotherTransaction = Convert.ToChar(Console.ReadLine());
-
-
-            } while(i < NumberOfTransaction);
-
+            switch (MathOperator)
+            { 
+                case '+':
+                    ComputationResult = FirstNumber + SecondNumber;
+                    break;
+                case '-':
+                    ComputationResult = FirstNumber - SecondNumber;
+                    break;
+                case '*':
+                    ComputationResult = FirstNumber * SecondNumber;
+                    break;
+                case '/':
+                    ComputationResult = FirstNumber / SecondNumber;
+                    break;
+            }
             return ComputationResult;
         }
     }
